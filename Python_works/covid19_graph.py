cad6f36ae2c642c7c69ef0fc5a02608fdd02e28d
EugeneNumata <69617793+EugeneNumata@users.noreply.github.com>
2022-04-04 09:38:34 +0900

    なし

--- Python_works/covid19_graph.py
+++ Python_works/covid19_graph.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pkg_resources import working_set
 
-covid19_data = r"C:\Users\eugem\iCloudDrive\pgm\portfolio\database\COVID19_Infected_jp.csv"
+covid19_data = r"..\source\COVID19_Infected_jp.csv"
 
 df = pd.read_csv(covid19_data, index_col=0, parse_dates=[0])
 

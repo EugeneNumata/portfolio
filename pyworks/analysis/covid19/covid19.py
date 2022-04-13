73c322b51c7ac43d834e67ddca9a96d07c6bf6d4
EugeneNumata <69617793+EugeneNumata@users.noreply.github.com>
2022-04-13 15:27:51 +0900

    プログラムの改変

--- pyworks/analysis/covid19/covid19.py
+++ pyworks/analysis/covid19/covid19.py
@@ -1,4 +1,5 @@
-# Ver 1.0 2022/04/12 
+# Ver 1.0 
+# 2022/04/12 
 # 更新内容
 # グラフで感染者と重症者の割合を出せるようにした。
 # 次は、グラフをもっと見やすくする(凡例をつける)ところから初めて、
@@ -7,11 +8,27 @@
 
 # ------------------
 
+# Ver 1.2
+# 2022/04/13
+# 更新内容
+# Y軸を2つにして重症者と新規感染者の推移を見やすく、予想しやすくした。
+# ここから未来を予想するには、
+# 状態空間モデルを使用するのがいいと思った。
+# Yt = H(Xt) + Wt
+# Xt=F(Xt-1)+ G(Vi)
+# Yt：時刻tの観測値。ベクトルの場合があるが、大抵はスカラー
+# Xt：時刻 tの状態ベクトル
+# H：状態を観測値に変換する関数。線形、非線形どちらの場合もあり、線形の場合は行列で表し、観測行列と呼ばれる
+# F：状態を次の状態に変換する関数。線形、非線形どちらの場合もあり、線形の場合は行列で表し、状態推移行列と呼ばれる
+# Wt：時刻 t の観測ノイズ
+# Vt：時刻 t の状態ノイズ
+# G：時刻 t の状態ノイズを変換する線形 or 非線形関数
+# 参考サイト
+# https://www.dskomei.com/entry/2021/01/22/181553
 
-
+# ------------------
 
 from cProfile import label
-from re import X
 from turtle import color
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -19,7 +36,7 @@ import numpy as np
 import datetime as dt
 from matplotlib.dates import DateFormatter
 import matplotlib.dates as mdates
-
+import matplotlib.cm as cm
 
 df = pd.read_csv(r".\source\covid19_db.csv")
 first_day = dt.date(2020,5,9)
@@ -29,17 +46,16 @@ for i in range(l):
     day = first_day + dt.timedelta(days=i)
     x.append(day)
 x_0 = np.linspace(0,l,l)
-y_1 = df["新規感染者"]
-y_2 = df["重症者"]
-y_3 = df["入院者"]
-y_4 = df["入院予定者"]
-y_5 = df["累計退院者"]
-y_6 = df["累計死者"]
-
-plt.plot(x,y_1,color="yellow",label="新規感染者")
-plt.plot(x,y_2,color="red",label="重症者")
-# plt.plot(x,y_3,color="orange",label="入院者")
-plt.plot(x,y_6,color="black",label="累計死者")
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%y/%m")) 
+y1 = df["new_infection"]
+y2 = df["new_severe"]
+fig = plt.figure()
+ax1 = fig.subplots()
+ax2 = ax1.twinx()
+ax1.plot(x,y1,color="yellow",label="New infection")
+ax2.plot(x,y2,color="red",label="New severe")
+h1, l1 = ax1.get_legend_handles_labels()
+h2, l2 = ax2.get_legend_handles_labels()
+ax1.legend(h1 + h2, l1 + l2)
+plt.title("New infection and new severe")
 plt.gcf().autofmt_xdate() 
 plt.show()
--- pyworks/analysis/covid19/covid19_test.py
+++ pyworks/analysis/covid19/covid19_test.py
@@ -1,20 +1,33 @@
-# from statistics import mean, median
-# from turtle import color
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# from pkg_resources import working_set
-
-# covid19_data = r"..\source\COVID19_Infected_jp.csv"
-
-# df = pd.read_csv(covid19_data, index_col=0, parse_dates=[0])
-
-
-# plt.plot(df["感染者全体"].resample("M").sum(),color="k")
-# plt.show()
-import numpy as np
+from cProfile import label
+from turtle import color
+import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import datetime as dt
+from matplotlib.dates import DateFormatter
+import matplotlib.dates as mdates
+import matplotlib.cm as cm
 
-x = np.linspace(0,1,100)
-y = x**2
-fig, axes = plt.subplots(2, 2, figsize=(20, 12))
+df = pd.read_csv(r".\source\covid19_db.csv")
+first_day = dt.date(2020,5,9)
+x = []
+l = len(df)
+for i in range(l):
+    day = first_day + dt.timedelta(days=i)
+    x.append(day)
+x_0 = np.linspace(0,l,l)
+y1 = df["new_infection"]
+fig = plt.figure()
+ax1 = fig.subplots()
+ax1.plot(x,y1,color="yellow",label="new_infection")
+h1, l1 = ax1.get_legend_handles_labels()
+plt.title("new_infection")
+plt.gcf().autofmt_xdate() 
 plt.show()
+y = pd.Series(
+    new_infection['#new_infection'].values, 
+    index=pd.to_datetime(new_infection['date'], infer_datetime_format=True)
+)
+y = y.astype(float)
+n_train = 120
+train_data, test_data = y.values[:n_train], y.values[n_train:]
